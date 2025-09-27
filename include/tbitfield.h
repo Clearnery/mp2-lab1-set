2bd0faf085802685cada36b8672ea6e1a492f63a
Artem <artemrybakov@outlook.com>
2025-09-27 23:01:53 +0300

    new_commit

--- include/tbitfield.h
+++ include/tbitfield.h
@@ -1,9 +1,4 @@
-// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
-//
-// tbitfield.h - Copyright (c) Гергель В.П. 07.05.2001
-//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
-//
-// Битовое поле
+
 
 #ifndef __BITFIELD_H__
 #define __BITFIELD_H__
@@ -12,24 +7,29 @@
 
 using namespace std;
 
-typedef unsigned int TELEM;
+
 
 class TBitField
 {
 private:
   int  BitLen; // длина битового поля - макс. к-во битов
-  TELEM *pMem; // память для представления битового поля
+  int *pMem; // память для представления битового поля
   int  MemLen; // к-во эл-тов Мем для представления бит.поля
 
-  // методы реализации
-  int   GetMemIndex(const int n) const; // индекс в pМем для бита n       (#О2)
-  TELEM GetMemMask (const int n) const; // битовая маска для бита n       (#О3)
+  // методы реализации]
+  int GetMemIndex(const int n) const; // индекс в pМем для бита n       (#О2)
+  int GetMemMask (const int n) const; // битовая маска для бита n 
+  int GetBitIndex(const int n) const;
 public:
+	
+	TBitField();
   TBitField(int len);                //                                   (#О1)
-  TBitField(const TBitField &bf);    //                                   (#П1)
+  TBitField(const TBitField &bf);
+  TBitField(TBitField&& bf);//                                   (#П1)
   ~TBitField();                      //                                    (#С)
 
   // доступ к битам
+  int GetBitLen() const;
   int GetLength(void) const;      // получить длину (к-во битов)           (#О)
   void SetBit(const int n);       // установить бит                       (#О4)
   void ClrBit(const int n);       // очистить бит                         (#П2)
